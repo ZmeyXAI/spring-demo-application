920792b17780852217bbd9fc6dfdad5cc079379e
Alex <alex.gora@mail.ru>
2014-09-22 14:30:25 +0300

    Revert "Revert "In Java configuration style""
    This reverts commit 6fbd4e126395b7f6b328de244d0d08c1bc0d9dfb.

---
+++ src/main/java/conspicuum/controllers/HelloController.java
@@ -0,0 +1,27 @@
+package conspicuum.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/*
+	Аннотоация @Controller необходима для того чтобы Spring знал, 
+что данный класс является bean’ом, и его  необходимо подгрузить 
+при старте приложения.
+	Аннотоация @RequestMapping(“/”) означает, что все методы 
+	будут получать запросы с URI, который будет начинаться 
+	строкой “/”. Чтобы метод welcome получал управление, 
+	необходимо выполнить запрос вида “/welcome”.
+*/
+
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+}
